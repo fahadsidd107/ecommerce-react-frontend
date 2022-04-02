ddd11073b506c143f48821d7020b0a0c04156229
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-03 01:37:19 +0500

    work complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Slider from './components/Slider';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
   <Home/>
+  <Slider/>
     </div>
   );
 }
