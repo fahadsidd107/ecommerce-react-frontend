8527607823d09972bf36a74624113473c2d1f4db
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-03 03:07:58 +0500

    work complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
 import './App.css';
-import Slider from './components/Slider';
 import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
   <Home/>
-  <Slider/>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
+import sliderItems from '../Data'
 
 function Home() {
   return (
     <div>
     <Navbar/>
+    <Slider id='id' img='img' title='title' desc='desc' bg='bg'/>
     </div>
   )
 }
