dbfe9cd603450845b6331df3c90bf7b2b8241e59
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-02 20:26:22 +0500

    work complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 
 const Container = styled.div`
   height: 60px;
@@ -8,15 +9,27 @@ const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 
 const Left = styled.div`
-flex:1;
+  flex: 1;
+  display: flex;
+  align-items: center;
 `;
 
 const Language = styled.span`
-font-size: 14px;
-cursor: pointer;
+  font-size: 14px;
+  cursor: pointer;
+
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  ${'' /* margin-left: 25px;
+  padding: 5px; */}
 `;
 
 
@@ -33,6 +46,10 @@ function Navbar() {
       <Wrapper>
       <Left>
       <Language>EN</Language>
+      <SearchContainer>
+      input
+      <Search />
+      </SearchContainer>
       </Left>
       <Center>Center</Center>
       <Right>Right</Right>
