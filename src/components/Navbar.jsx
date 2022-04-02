67ab80da556a6fe8e732a3f72a5b2ff9c55f1e52
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-02 20:43:32 +0500

    work complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import { Badge } from "@material-ui/core";
 
 const Container = styled.div`
   height: 60px;
@@ -21,38 +22,65 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
-
 `;
 
 const SearchContainer = styled.div`
   border: 0.5px solid lightgray;
   display: flex;
   align-items: center;
-  ${'' /* margin-left: 25px;
-  padding: 5px; */}
+  margin-left: 25px;
+  padding: 5px;
 `;
 
+const Input = styled.input`
+  border: none;
+`;
 
 const Center = styled.div`
-flex:1;
+flex: 1;
+text-align: center;
 `;
+
+const Logo = styled.h1`
+font-weight: bold;
+`;
+
 const Right = styled.div`
-flex:1;
+  flex: 1;
+  display: flex;
+  align-items: center;
+    justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+font-size:14px;
+cursor:pointer;
+margin-left: 25px;
 `;
 
 function Navbar() {
   return (
     <Container>
       <Wrapper>
-      <Left>
-      <Language>EN</Language>
-      <SearchContainer>
-      input
-      <Search />
-      </SearchContainer>
-      </Left>
-      <Center>Center</Center>
-      <Right>Right</Right>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input placeholder="Search" />
+            <Search style={{ color: "gray", fontSize: 16 }}/>
+          </SearchContainer>
+        </Left>
+        <Center>
+            <Logo>FAHAD.</Logo>
+        </Center>
+        <Right>
+        <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+          <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
