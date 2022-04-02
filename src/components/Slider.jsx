fbfdfb3b350fad525a7ebc7e2248ae342ba7f751
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-03 02:24:43 +0500

    work complete

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,8 +1,51 @@
+import { ArrowLeftOutlined,ArrowRightOutlined } from '@material-ui/icons';
 import React from 'react'
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100%;
+    height: 93.42825848849945vh;
+    display: flex;
+    background-color: coral;
+    position: relative;
+`;
+const Arrow = styled.div`
+width:50px;
+height:50px;
+background-color:#fff7f7;
+border-radius:50%;
+display:flex;
+align-items:center;
+justify-content:center;
+position:absolute;
+top:0;
+bottom:0;
+margin:auto;
+opacity:0.7;
+left: ${props => props.direction === 'left' && '15px'};
+right: ${props => props.direction === 'right' && '15px'};
+`;
+
+const Wrapper = styled.div`
+height:100%;
+background-color:pink;
+`;
+
+
 
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <Container>
+    <Arrow direction="left">
+        <ArrowLeftOutlined/>
+    </Arrow>
+    <Wrapper>
+
+    </Wrapper>
+    <Arrow direction="right">
+        <ArrowRightOutlined/>
+    </Arrow>
+    </Container>
   )
 }
 
