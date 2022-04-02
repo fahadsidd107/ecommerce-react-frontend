e0b37cd2d2a7b9119b5f37a2f00e2f62e6b6301b
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-02 19:34:48 +0500

    work complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const Container = styled.div`
+  background-color: #000;
+  `
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <Container>Navbar</Container>
   )
 }
 
