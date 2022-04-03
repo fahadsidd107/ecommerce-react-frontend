abc119b19ed9209906260db6a033f3d8e83f9775
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-04 00:38:51 +0500

    work complete

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -4,11 +4,19 @@ import styled from "styled-components";
 const Container = styled.div`
   flex: 1;`;
 
+const Img = styled.image``;
+const Title = styled.h1``;
+const Info = styled.div``;
+const Button = styled.button``;
 
 const CategoryItem = ({ item }) => {
   return (
     <Container>
-    CategoryItem
+    <Img src={item.img}/>
+    <Info>
+    <Title>{item.title}</Title>
+    <Button>SHOP NOW</Button>
+    </Info>
     </Container>
   )
 }
