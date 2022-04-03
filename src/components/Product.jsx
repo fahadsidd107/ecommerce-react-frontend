ea4ea18ffc36a261a60dd51e3ad30ba72d6bd097
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-04 01:53:35 +0500

    work complete

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,6 +3,22 @@ import React from 'react'
 // import {  } from "@mui/icons-material";
 import { FavoriteBorderOutlined, SearchOutlined,ShoppingCartOutlined } from "@material-ui/icons";
 
+const Info = styled.div`
+opacity: 0;
+position:absolute;
+width:100%;
+height:100%;
+top:0;
+left:0;
+background-color:rgba(0,0,0,0.2);
+z-index:3;
+display:flex;
+align-items:center;
+justify-content:center;
+transition:all 0.5s ease;
+cursor:pointer;
+`;
+
 const Container = styled.div`
 flex:1;
 margin:5px;
@@ -13,6 +29,10 @@ align-items:center;
 justify-content:center;
 background-color:#f5fbfd;
 position:relative;
+
+&:hover ${Info}{
+opacity:1;
+}
 `;
 
 const Circle = styled.div`
@@ -26,19 +46,7 @@ const Image = styled.img`
 height:75%;
 z-index:2;
 `;
-const Info = styled.div`
-position:absolute;
-width:100%;
-height:100%;
-top:0;
-left:0;
-background-color:rgba(0,0,0,0.2);
-z-index:3;
-display:flex;
-align-items:center;
-justify-content:center;
 
-`;
 const Icon = styled.div`
 width:40px;
 height:40px;
