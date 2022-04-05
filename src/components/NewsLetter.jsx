9229054b019cba4ab1252a06f7b8d3dccb818a51
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-05 18:35:39 +0500

    work complete

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -1,11 +1,18 @@
+import { Send } from '@material-ui/icons';
 import React from 'react'
+import styled from "styled-components";
 
 const NewsLetter = () => {
   return (
     <Container>
 <Title></Title>
 <Description></Description>
-<InputContainer></InputContainer>
+<InputContainer>
+    <Input/>
+    <Button>
+        <Send/>
+    </Button>
+</InputContainer>
     </Container>
   )
 }
