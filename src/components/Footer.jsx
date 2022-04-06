f8105fe96d5e03655701d5f4ffdae43c5227e0ff
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-06 21:39:34 +0500

    work complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,55 @@
+import { Facebook, Instagram, Pinterest, Twitter } from '@material-ui/icons';
 import React from 'react'
 import styled from "styled-components";
 
+const Container = styled.div`
+display: flex;
+justify-content: space-between;
+`;
+
+
+const Left = styled.div`
+flex:1;
+`;
+
+const Logo = styled.h1``;
+
+const Description = styled.p``;
+
+
+const Social = styled.div``;
+
+const SocialIcon = styled.div``;
+
+const Center = styled.div`
+flex:1;
+`;
+
+const Right = styled.div`
+flex:1;
+`;
+
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Container>
+    <Left>
+        <Logo>FAHAD.</Logo>
+        <Description>
+        FAHAD first made waves in Pakistan's e-commerce market after its introduction in 2012.
+         We have since grown to become Pakistan's largest platform for online shopping with a 
+         network spread across Asia in Pakistan, Bangladesh, Sri Lanka .
+        </Description>
+        <Social>
+            <SocialIcon><Facebook/></SocialIcon>
+            <SocialIcon><Instagram/></SocialIcon>
+            <SocialIcon><Twitter/></SocialIcon>
+            <SocialIcon><Pinterest/></SocialIcon>
+        </Social>
+    </Left>
+    <Center>2</Center>
+    <Right>3</Right>
+    </Container>
   )
 }
 
