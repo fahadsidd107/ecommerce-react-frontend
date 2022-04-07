b0dc6a32b2eab970ec8b88eba9577108b6eef98f
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-07 22:04:24 +0500

    work complete

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 import Navbar from '../components/Navbar';
 import Announcement from '../components/Announcement';
-import Products from '../components/Product';
+import Products from '../components/Products'
+import NewsLetter from '../components/NewsLetter';
+import Footer from '../components/Footer';
 
 const Container = styled.div``;
 
@@ -18,6 +20,16 @@ const Filter = styled.div`
 margin:20px;
 `;
 
+const FilterText = styled.span`
+font-size:20px;
+font-weight:600;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
+const Option = styled.option``
 
 const ProductList = () => {
   return (
@@ -26,11 +38,12 @@ const ProductList = () => {
         <Announcement/>
         <Title>Dresses</Title>
         <FilterContainer>
-<Filter>Filter1</Filter>
-<Filter>Filter2</Filter>
+<Filter><FilterText>Filter Products :</FilterText></Filter>
+<Filter><FilterText>Sort Products :</FilterText></Filter>
         </FilterContainer>
-        <Products/> 
-
+         <Products/> 
+         <NewsLetter/>
+         <Footer/>
     </Container>
   )
 }
