f3186e6b0f986dd5364c3a1e3c8154d1923d9e77
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-07 21:38:16 +0500

    work complete

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
 import Navbar from '../components/Navbar';
+import Announcement from '../components/Announcement';
 
 const Container = styled.div``;
+
+const Title = styled.h1`
+margin:20px`;
+
+const FilterContainer = styled.div`
+display:flex;
+justify-content:space-between;
+`;
+
+const Filter = styled.div`
+margin:20px;
+`;
+
 
 const ProductList = () => {
   return (
     <Container>
         <Navbar/>
+        <Announcement/>
+        <Title>Dresses</Title>
+        <FilterContainer>
+<Filter>Filter1</Filter>
+<Filter>Filter2</Filter>
+        </FilterContainer>
     </Container>
   )
 }
