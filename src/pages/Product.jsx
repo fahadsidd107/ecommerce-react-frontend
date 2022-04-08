58fe0c1bd50327a3a86bac5480b54f8a0ad70455
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-08 20:52:36 +0500

    work complete

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import styled from "styled-components";
 import Announcement from '../components/Announcement';
@@ -7,11 +8,28 @@ import NewsLetter from '../components/NewsLetter';
 
 const Container=styled.div``;
 
+const Wrapper=styled.div``;
+
+const ImgContainer = styled.div``;
+
+const Image = styled.img``;
+
+const InfoContainer = styled.div``;
+
+const Title = styled.h1``;
+
+const Description = styled.div``;
+
+
 const Product = () => {
   return (
     <Container>
       <Navbar/>
       <Announcement/>
+      <Wrapper>
+        <ImgContainer><Image></Image></ImgContainer>
+        <InfoContainer><Title><Description></Description></Title></InfoContainer>
+      </Wrapper>
       <NewsLetter/>
       <Footer/>
     </Container>
