931ace94162af10cb8cf12d246a2af2cb8163f90
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-08 21:12:45 +0500

    work complete

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -25,14 +25,55 @@ object-fit:cover;
 
 const InfoContainer = styled.div`
 flex:1;
+padding:0 50px;
 `;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+font-weight:200;
+`;
 
-const Description = styled.p``;
-const Price = styled.span``;
+const Description = styled.p`
+margin:20px 0px;
+`;
 
+const Price = styled.span`
+font-weight:100;
+font-size:40px;
+`;
 
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+
+`;
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+
+const FilterSizeOption = styled.option``;
 const Product = () => {
   return (
     <Container>
@@ -50,6 +91,24 @@ const Product = () => {
             tristique tortor pretium ut. Curabitur elit justo, consequat id
             condimentum ac, volutpat ornare.</Description>
             <Price>Rs.1500</Price>
+            <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="darkblue" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+            </FilterContainer>
         </InfoContainer>
       </Wrapper>
       <NewsLetter/>
