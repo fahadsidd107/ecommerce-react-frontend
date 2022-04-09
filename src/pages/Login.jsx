885c2bbee625c0a5675d9e6e8fcdd132536800d1
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-10 02:50:22 +0500

    work complete

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -58,14 +58,10 @@ const Login = () => {
   return (
     <Container>
      <Wrapper>
-        <Title>CREATE AN ACCOUNT</Title>
+        <Title>SIGN IN</Title>
         <Form>
-          <Input placeholder="First Name" />
-          <Input placeholder="Last Name" />
-          <Input placeholder="Username" />
           <Input placeholder="Email" />
           <Input placeholder="Password" />
-          <Input placeholder="Confirm Password" />
           <Agreement>
             By creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
