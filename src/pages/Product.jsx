05ca2f5b985b80d0c219dace477f009499183acf
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-09 19:20:01 +0500

    work complete

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -96,7 +96,16 @@ align-items:center;
 justify-content:center;
 margin:0px 5px;
 `;
-const Button = styled.button``;
+const Button = styled.button`
+padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+  &:hover{
+      background-color: #f8f4f4;
+  }
+`;
 
 
 
