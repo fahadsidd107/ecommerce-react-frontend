2eb62bb393d64fafb300415b258b0070bf84f0ad
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-10 02:25:08 +0500

    work complete

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,13 +5,21 @@ const Container=styled.div``;
 const Wrapper=styled.div``;
 const Title=styled.h1``;
 const Form=styled.form``;
+const Input=styled.input``;
 
 const Register = () => {
   return (
     <Container>
         <Wrapper>
             <Title>CREATE AN ACCOUNT</Title>
-            <Form></Form>
+            <Form>
+              <Input placeholder='First Name'/>
+              <Input placeholder='Last Name'/>
+              <Input placeholder='Username'/>
+              <Input placeholder='Email'/>
+              <Input placeholder='Password'/>
+              <Input placeholder='Confirm Password'/>
+            </Form>
         </Wrapper>
     </Container>
   )
