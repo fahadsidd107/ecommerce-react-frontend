501a4cdfd5710ea5a4ba4242ff9a4630b72616c0
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-10 02:31:05 +0500

    work complete

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import styled from "styled-components";
 
-const Container=styled.div``;
+const Container=styled.div`
+width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 const Wrapper=styled.div``;
 const Title=styled.h1``;
 const Form=styled.form``;
