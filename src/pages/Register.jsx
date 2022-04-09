066a9541ea32152636e61416faae935b1822b4c6
fahadsidd107 <fsiddiqui107@gmail.com>
2022-04-10 02:36:31 +0500

    work complete

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
 
-const Container=styled.div`
-width: 100vw;
+const Container = styled.div`
+  width: 100vw;
   height: 100vh;
   background: linear-gradient(
       rgba(255, 255, 255, 0.5),
@@ -15,34 +15,42 @@ width: 100vw;
   align-items: center;
   justify-content: center;
 `;
-const Wrapper=styled.div``;
-const Title=styled.h1``;
-const Form=styled.form``;
-const Input=styled.input``;
+
+const Wrapper = styled.div`
+  width: 40%;
+  padding: 20px;
+  background:white;
+`;
+const Title = styled.h1`
+font-size:24px;
+`;
+
+const Form = styled.form``;
+const Input = styled.input``;
 const Agreement = styled.span``;
 const Button = styled.button``;
 
 const Register = () => {
   return (
     <Container>
-        <Wrapper>
-            <Title>CREATE AN ACCOUNT</Title>
-            <Form>
-              <Input placeholder='First Name'/>
-              <Input placeholder='Last Name'/>
-              <Input placeholder='Username'/>
-              <Input placeholder='Email'/>
-              <Input placeholder='Password'/>
-              <Input placeholder='Confirm Password'/>
-              <Agreement>
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>
+        <Form>
+          <Input placeholder="First Name" />
+          <Input placeholder="Last Name" />
+          <Input placeholder="Username" />
+          <Input placeholder="Email" />
+          <Input placeholder="Password" />
+          <Input placeholder="Confirm Password" />
+          <Agreement>
             By creating an account, I consent to the processing of my personal
             data in accordance with the <b>PRIVACY POLICY</b>
           </Agreement>
           <Button>CREATE</Button>
-            </Form>
-        </Wrapper>
+        </Form>
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
